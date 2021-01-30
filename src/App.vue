0977aa7e38759257cacb0462eac261e4fd092210
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-01-31 02:10:51 +0530

    added functionality for time

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,16 @@
 </template>
 
 <style>
+* {
+  background-color: #2C2F33;
+  color: #878787;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
@@ -21,7 +25,6 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
--- src/components/Typing.vue
+++ src/components/Typing.vue
@@ -9,7 +9,6 @@
                 <span @click="set(60, 'time')">60s</span>
                 /
                 <span @click="set(120, 'time')">120s</span>
-                /
             </div>
             <div id="word">
                 <span @click="set(10, 'word')">10 Words</span>
@@ -19,13 +18,15 @@
                 <span @click="set(50, 'word')">50 Words</span>
                 /
                 <span @click="set(100, 'word')">100 Words</span>
-                /
             </div>
-        </div>
-        <div id="words"></div>
-        <div id="typing-area">
-            <input type="text" id="typing-input" placeholder="Start typing..." autocomplete="off" spellcheck="false" tabindex="2" autofocus>
-            <button id="redo" tabindex="1">Redo</button>
+            <div id="typing-area">
+                <div id="score"></div>
+                <div id="words"></div>
+                <div id="typing-area">
+                    <input type="text" id="typing-input" @input="onType()" placeholder="Start typing..." autocomplete="off" spellcheck="false" tabindex="2" autofocus>
+                    <button id="redo" tabindex="1">Redo</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -37,7 +38,8 @@ export default {
     name: "Typing",
     data: function() {
         return {
-            
+            time: null,
+            type: null
         }
     },
     mounted() {
@@ -48,21 +50,63 @@ export default {
             if (type === "word") {
                 console.log(amt, type)
                 let words = ""
+                this.type = type;
                 for (let i = 0; i < amt; i++) {
                     const index = Math.floor(Math.random() * wordsList["english"].length);
                     words += wordsList["english"][index] + " "
                 }
-                document.getElementById("words").innerHTML = words
+                document.getElementById("words").innerHTML = words;
+                document.getElementById("score").innerHTML = `0/${amt}`;
             } 
             else if (type === "time") {
-                console.log(amt, type)
+                console.log(amt, type) 
+                let words = ""
+                this.time = parseInt(amt);
+                this.type = type;
+
+                for (let i = 0; i < amt * 4; i++) {
+                    const index = Math.floor(Math.random() * wordsList["english"].length);
+                    words += wordsList["english"][index] + " "
+                }
+                document.getElementById("words").innerHTML = words;
+                document.getElementById("score").innerHTML = `${amt}s`;
             }
-        }
+        },
+        onType() {
+            console.log("started")
+            if (this.type === "time") {
+                console.time('looper');
+                const countdown = setInterval(() => {
+                    this.time -= 1;
+                    document.getElementById("score").innerHTML = `${this.time}s`;
+                    if (this.time === 0) {
+                        clearInterval(countdown)
+                    }
+                    console.timeLog('looper');
+            }, 1000)
+            }
+        },
     }
 }
 </script>
 
 <style scoped>
+    #typing-area {
+        margin-top: 5vh;
+    }
+
+    #words {
+        display: inline-block;
+        padding: 10px;
+        font-size: 18px;
+        max-width: 75vw;
+    }
+
+    #score {
+        margin-bottom: 5vh;
+        font-size: 48px;
+    }
+
     #config span {
         cursor: pointer;
     }

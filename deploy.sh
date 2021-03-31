681f24d3cd12a192090e52220b1d24ba0f420c71
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-03-31 16:14:29 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ npm run build
 # navigate into the build output directory
 cd docs
 
+# if you are deploying to a custom domain
+echo 'www.dinotype.ml' > CNAME
+cd -
 git add .
 git commit -m 'deploy'
 git push
 
 
-cd -
